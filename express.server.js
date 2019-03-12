a7f05ff26ce8469ece82f8552045563f25b884e5
Sagal Mohamed <sagalmohamed@Sagals-MacBook-Pro.local>
2019-03-12 19:49:33 -0400

    created shortcut

--- express.server.js
+++ express.server.js
@@ -22,7 +22,25 @@ app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
   });
 
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  console.log('db================', templateVars)
+  res.render("urls_index", templateVars);
+});
 
+// /urls/b2xVn2 => http://www.lighthouselabs.ca
+// /urls/9sm5xK => http://www.google.com
+
+// /users/:id/posts/:post_id
+// /users/1/posts/3
+// params = { id: 1, post_id: 3}
+// User.find(req.params.id)
+// postMessage.find(req.params.post_id)
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
